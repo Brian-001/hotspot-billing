af9b7f653f10218e5bb8e3d8a8520034983f6763
Brian <karanjabrian001@gmail.com>
2025-03-31 10:27:56 +0300

    Added MpesaController

---
+++ app/Models/HotspotPlan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HotspotPlan extends Model
+{
+    //
+    protected $fillable = ['name', 'price', 'duration'];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\HotspotPlan;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        HotspotPlan::insert([
+            ['name' => '1 Hour', 'price' => 5, 'duration' => 60],
+            ['name' => '3 Hour', 'price' => 20, 'duration' => 180],
+            ['name' => '12 Hour', 'price' => 30, 'duration' => 720],
+            ['name' => '24 Hour', 'price' => 40, 'duration' => 1440],
         ]);
     }
 }
